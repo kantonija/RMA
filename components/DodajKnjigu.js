d5e7f5ca613846154f14811904224caac137ec74
Antonija Kožul <151862304+kantonija@users.noreply.github.com>
2025-01-03 17:58:55 +0100

    Dodana stranica DodajKnjigu

--- components/DodajKnjigu.js
+++ components/DodajKnjigu.js
@@ -1,12 +1,113 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from "react-native";
+import PageDesign from "./ui/PageDesign";
 
-const DodajKnjigu = () => {
+const PronadjiKnjiznicu = () => {
   return (
-    <View>
-      <Text>DodajKnjigu</Text>
-    </View>
+    <PageDesign>
+      <TextInput
+        style={styles.input1}
+        placeholder="Unesite ime knjige"
+        value={""} />
+        <TextInput
+        style={styles.input2}
+        placeholder="Unesite ime pisca"
+        value={""} />
+        <TextInput
+        style={styles.input3}
+        placeholder="Unesite broj stranica"
+        value={""} />
+        <TextInput
+        style={styles.input4}
+        placeholder="Unesite žanr knjige"
+        value={""} />
+        <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>   Učitaj fotografiju</Text>
+      </TouchableOpacity>
+      <View
+              style={{
+                width: 150,
+                height: 150,
+                backgroundColor: "white",
+                borderRadius: 10,
+                borderColor: "#ddd",
+                borderWidth: 1,
+                justifyContent: "center",
+                alignItems: "center",
+                top: "-13%",
+                left: "-18%",	
+              }}
+            >
+            </View>
+    </PageDesign>
   );
 };
 
-export default DodajKnjigu;
+const styles = StyleSheet.create({
+  input1: {
+    width: "70%",
+    top: "-10%",
+    height: 40,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    backgroundColor: "#fff",
+    fontSize: 14,
+    zIndex: 3,
+  },
+  input2: {
+    width: "70%",
+    top: "-9%",
+    height: 40,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    backgroundColor: "#fff",
+    fontSize: 14,
+    zIndex: 3,
+  },
+  input3: {
+    width: "70%",
+    top: "-8%",
+    height: 40,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    backgroundColor: "#fff",
+    fontSize: 14,
+    zIndex: 3,
+  },
+  input4: {
+    width: "70%",
+    top: "-7%",
+    height: 40,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    backgroundColor: "#fff",
+    fontSize: 14,
+    zIndex: 3,
+  },
+  button: {
+    top: "9%",
+    right: -110,
+    width: "25%",
+    height: 60,
+    borderColor: "transparent",
+    backgroundColor: "transparent",
+    justifyContent: "center",
+    alignItems: "center",
+    zIndex: 4,
+  },
+  buttonText: {
+    color: "##6c4255",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
+
+export default PronadjiKnjiznicu;
--- components/ui/PageDesign.js
+++ components/ui/PageDesign.js
@@ -26,8 +26,8 @@ const styles = StyleSheet.create({
     },
     content: {
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center',
+        justifyContent: 'center',
     },
   circle1: {
     flex: 1,
